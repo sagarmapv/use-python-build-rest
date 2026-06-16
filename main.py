aca44ef947b3ea49782ef7126108256ba2b1266c
Aditya Madduri <sagarmapv@gmail.com>
2026-06-16 06:27:24 +0530

    Fix port 8000 -> 8004 to avoid clash with local k8s gateway on WSL

--- main.py
+++ main.py
@@ -76,4 +76,4 @@ def delete_item(item_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8004, reload=True)
